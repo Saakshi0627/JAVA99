bd81da3624fa2278b9bbc2df2d719862fd4cb248
Saakshi0627 <158995200+Saakshi0627@users.noreply.github.com>
2024-02-08 10:41:17 +0530

    Example of short variable.
    This program is an example of short variable where I have declared and initialized the short variable and printed it.

---
+++ Short_Variable_Program.java
@@ -0,0 +1,19 @@
+class Short_Variable_Program
+{
+
+//this is main method
+public static void main(String[]svp)
+{
+
+//this is declaration and initialization of variable
+short var01= 561;
+
+//print
+System.out.println(var01);
+}
+
+}
+
+/*Output:
+Compile time:- Sucessful
+Run time:- 561*/
