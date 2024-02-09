4d4574463c4d08f95d25f35fa50beb26908858fe
Saakshi0627 <158995200+Saakshi0627@users.noreply.github.com>
2024-02-09 10:08:28 +0530

    Example of String Variable
    I have declared and initialized the string variable and then printed it.

---
+++ String_Variable_Program.java
@@ -0,0 +1,20 @@
+class String_Variable_Program
+{
+
+public static void main(String[]svp)
+{
+
+String xyz= "Sakshiii";
+String abc= "Sahil";
+System.out.println(xyz);
+System.out.println(abc);
+
+}
+
+}
+
+
+/*Output:
+Compile time:- Sucessful
+Run time:- Sakshiii
+           Sahil */
