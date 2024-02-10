946c03e3134df9ef8959712f92dd5e3a0114f66b
Saakshi0627 <158995200+Saakshi0627@users.noreply.github.com>
2024-02-10 11:13:45 +0530

    Basic Unary Operator Program
    It is an example of how unary operator works.

---
+++ UnaryExample.java
@@ -0,0 +1,26 @@
+class UnaryExample
+{
+
+public static void main(String[]args)
+
+{
+int a=27;
+
+System.out.println(a++);
+
+System.out.println(++a);
+
+System.out.println(a);
+
+System.out.println(++a);
+}
+
+}
+
+/*Output:
+Compile time:Sucessful
+Runtime:27
+        29
+        29
+        30
+*/
