f0d5e4baaf9c4b40209056da4b45db315745706e
Saakshi0627 <158995200+Saakshi0627@users.noreply.github.com>
2024-02-16 20:49:14 +0530

    Ternary operator example
    Using Ternary Operator I have checked where the given person age is eligible for voting or not.

---
+++ Voting1234.java
@@ -0,0 +1,12 @@
+//Program to check where the age gien is eligible for voting using ternary operator
+
+class Voting1234
+{
+public static void main(String[]args)
+{
+int age= 30;
+ 
+String result= (age>=18) ? ("Eligible for Voting") : ("Not Eligible for Voting");
+ System.out.println(result);
+}
+}
