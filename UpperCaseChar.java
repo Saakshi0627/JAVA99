ecf8101612ba3824c79c87ecf9f91d9eb3f31945
Saakshi0627 <158995200+Saakshi0627@users.noreply.github.com>
2024-02-21 23:26:07 +0530

    Ternary Operator
    To check whether the given char is in Lower or Upper case.

---
+++ UpperCaseChar.java
@@ -0,0 +1,22 @@
+// To check whether thr character is in Upper case or in Lower case.
+
+
+class UpperCaseChar
+{
+
+public static void main(String[]args)
+
+{
+
+char ch= 'a';
+
+String output= (ch>=65 && ch<=90) ? ("The char is in Upper case.") : ("The char is in Lower case.");
+
+System.out.println(output);
+
+}
+
+}
+
+
+//Output: The char is in Lower case.
